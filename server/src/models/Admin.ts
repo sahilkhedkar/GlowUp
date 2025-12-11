95c9a601309e21884fccc3fe38c828ee9e03fd6e
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-12-11 09:46:02 +0530

    written Admin Schema

---
+++ server/src/models/Admin.ts
@@ -0,0 +1,18 @@
+import mongoose, {Schema} from "mongoose";
+
+const adminSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+export const AdminModel = mongoose.model('Admin', adminSchema)
