444359096e03cda9d506900dfc44dbecd8bf7670
Sahil Khedkar <sahilkhedkar11@gmail.com>
2025-12-11 15:04:57 +0530

    made some changes in index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config({
-    path: "./.env"
+  path: "./.env",
 });
 
 const app = express();
@@ -11,9 +12,19 @@ const Port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const dburl = process.env.MONGO_URI!;
+
+const main = async () => {
+  await mongoose.connect(dburl);
+};
+
+main()
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(Port, () => {
   console.log("Server is running on port 3000");
